4e846ca54c2d5ff4f6f5543022e88dfee0c28b96
JagrutiNaikare <jagrutinaikare@gmail.com>
2022-06-07 23:00:24 +0530

    DataSeat JUnit

---
+++ src/test/java/com/dataseat/testcases/ClassTestSuite.java
@@ -0,0 +1,13 @@
+package com.dataseat.testcases;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+ 
+@RunWith(Suite.class)
+@SuiteClasses({ TestCases.class})
+
+public class ClassTestSuite {
+
+
+}
